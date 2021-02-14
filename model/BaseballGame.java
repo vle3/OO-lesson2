305eb67d82f1f7305ac60ea549cb1c0eca1b51c4
Vincent Le <vle3.uco.edu>
2021-02-14 16:44:10 -0600

    finish set up test

--- model/BaseballGame.java
+++ model/BaseballGame.java
@@ -14,6 +14,48 @@ public class BaseballGame
         generateKey();
     }
 
+    public BaseballGame(int k0 , int k1 , int k2)
+    {
+        key[0] = k0;
+        key[1] = k1;
+        key[2] = k2;
+    }
+
+    public void setGuess(int pos, int value)
+    {
+        assert 0 <= pos && pos < 3 : "setGuess out of range in position";
+        guess[pos] = value;
+    }
+
+    public void computeBallsStrikes()
+    {
+        strikeCount = 0 ; 
+        for (int i = 0 ; i < 3 ; i++ )
+        {
+            if(key[i] == guess[i]) ++strikeCount;
+        }
+
+        ballCount = 0 ; 
+        for(int i = 0 ; i < 3 ; i++)
+        {
+            for(int n = 0; n < 3 ; n++)
+            {
+                if(i == n) continue;
+                if(key[i] == guess[n]) ballCount++;
+            }
+        }
+    }
+
+    public int getBallCount()
+    {
+        return ballCount;
+    }
+
+    public int getStrikeCount()
+    {
+        return strikeCount;
+    }
+
     private void generateKey()
     {
         Random r = new Random();
--- test/BaseballGameTest.java
+++ test/BaseballGameTest.java
@@ -13,7 +13,62 @@ public class BaseballGameTest
             BaseballGame b = new BaseballGame();
             assert checkKey(b.getKey());
         }
+
+        BaseballGame b1 = new BaseballGame( 0 , 1 , 2);
+        b1.setGuess(0,1);
+        b1.setGuess(1,5);
+        b1.setGuess(2,9);
+        b1.computeBallsStrikes();;
+        assert b1.getBallCount() == 1;
+        assert b1.getStrikeCount() == 0;
+
+        b1 = new BaseballGame( 0 , 1 , 2);
+        b1.setGuess(0,1);
+        b1.setGuess(1,0);
+        b1.setGuess(2,2);
+        b1.computeBallsStrikes();;
+        assert b1.getBallCount() == 2;
+        assert b1.getStrikeCount() == 1;
+
+        b1 = new BaseballGame( 0 , 1 , 2);
+        b1.setGuess(0,5);
+        b1.setGuess(1,6);
+        b1.setGuess(2,9);
+        b1.computeBallsStrikes();;
+        assert b1.getBallCount() == 0;
+        assert b1.getStrikeCount() == 0;
+
+        ballStrikeCountCheck(1, 2, 3, 0, 2, 5, 0, 1);
+        ballStrikeCountCheck(2, 1, 3, 6, 1, 5, 0, 1);
+        ballStrikeCountCheck(1, 2, 3, 8, 6, 3, 0, 1);
+        ballStrikeCountCheck(1, 2, 3, 8, 1, 0, 1, 0);
+        ballStrikeCountCheck(1, 2, 3, 2, 0, 8, 1, 0);
+        ballStrikeCountCheck(1, 2, 3, 3, 0, 8, 1, 0);
+        ballStrikeCountCheck(1, 2, 3, 3, 1, 8, 2, 0);
+        ballStrikeCountCheck(1, 2, 3, 1, 2, 3, 0, 3);
     }    
+
+    private static boolean ballStrikeCountCheck(int k0 , int k1 , int k2 , int g0 , int g1 , int g2 , int balls , int strikes)
+    {
+        BaseballGame b = new BaseballGame(k0, k1, k2);
+        b.setGuess(0, g0);
+        b.setGuess(1, g1);
+        b.setGuess(2, g2);
+        b.computeBallsStrikes();
+        if (b.getBallCount() == balls && b.getStrikeCount() == strikes)
+        {
+            return true;
+        }
+        else 
+        {
+            String keys = "" + k0 + k1 + k2;
+            String guesses = "" + g0 + g1 + g2;
+            System.out.println("Key: " + keys + "Guesses: " + guesses + " Incorrect count");
+            System.out.println("Balls: " + b.getBallCount() + " Strikes: " + b.getStrikeCount());
+            return false;
+        }
+
+    }
 
     private static boolean checkKey(int[] key)
     {
